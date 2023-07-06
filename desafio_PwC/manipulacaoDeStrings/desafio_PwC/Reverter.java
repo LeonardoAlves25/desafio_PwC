6f378ca9d231da095a511a16075632fa2e6d197f
Leonardo <leonardoalves5861@gmail.com>
2023-07-06 16:21:57 -0300

    Commit initial

---
+++ desafio_PwC/manipulacaoDeStrings/desafio_PwC/Reverter.java
@@ -0,0 +1,13 @@
+package desafio_PwC;
+
+public class Reverter {
+	
+	public static void main(String[] args) {
+		
+	}
+}
+
+
+/*1. Reverta a ordem das palavras nas frases, mantendo a ordem das pelavras.
+a. Input "Hello, World! OpenAI is amazing."
+b Output "amazing is OpenAI World! Hello,"*/
