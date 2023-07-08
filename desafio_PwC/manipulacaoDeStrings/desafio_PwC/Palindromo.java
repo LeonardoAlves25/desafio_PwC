94ecb5a255bbc175c29ac1989210503a89bf12bd
usuario <usuario@WINPC-03.lan>
2023-07-08 20:50:23 -0300

    Implementada as condições de palavras palindromas e mais próximas

--- desafio_PwC/manipulacaoDeStrings/desafio_PwC/Palindromo.java
+++ desafio_PwC/manipulacaoDeStrings/desafio_PwC/Palindromo.java
@@ -5,14 +5,33 @@ import java.util.Scanner;
 public class Palindromo {
 	 
 	public static void main(String[] args) {
+	
+		//ufa! Essa deu trabalho
 		
 		Scanner sc = new Scanner(System.in);
 		
 		System.out.println("Digite a palavra que deseja verificar: ");
 		String input = sc.next();
-		String rev = input;
+		String rev = "";
+		String comp = "";
 		
-		System.out.println(rev);
+		for (int i = input.length() -1 ; i >= 0; i--) {
+			rev = rev+input.charAt(i);
+		}
+		
+		for (int j = 0; j <= input.length() -1; j++) {
+			if (input.charAt(j) == rev.charAt(j)) {
+				comp = comp+rev.charAt(j);	
+			}
+		}
+		
+		System.out.println("\nA palavra reversa é: "+rev);
+		
+		if(input.equals(rev)) System.out.println("\nEsta palavra é palindroma!");
+		else System.out.println("\nEsta palavra não é palindroma");
+
+		if(comp != "")System.out.println("\nO palindromo mais próximo é: "+comp);
+		else System.out.println("\nNão existe palindromo mais próximo");
 		sc.close();
 	}
 }
