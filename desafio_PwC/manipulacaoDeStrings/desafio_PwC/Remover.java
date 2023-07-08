c17df8353a96a33412e10bc4a09c34e8779533d0
usuario <usuario@WINPC-03.lan>
2023-07-08 15:41:33 -0300

    Commit initial

---
+++ desafio_PwC/manipulacaoDeStrings/desafio_PwC/Remover.java
@@ -0,0 +1,17 @@
+package desafio_PwC;
+
+public class Remover {
+	
+	public static void main(String[] args) {
+		
+		String s = "Hello, World!";
+		
+		System.out.println(s);
+	}
+}
+
+
+//Remova todos os caracteres duplicados
+//na string abaixo: 
+//Input: "Hello, World!"
+//Output: "Helo, Wrd!"
