d103c5d0171946283cdc4e454236cdc179a9bc3d
usuario <usuario@WINPC-03.lan>
2023-07-08 16:47:46 -0300

    Vereficação de caracteres repetidos

--- desafio_PwC/manipulacaoDeStrings/desafio_PwC/Remover.java
+++ desafio_PwC/manipulacaoDeStrings/desafio_PwC/Remover.java
@@ -1,17 +1,28 @@
 package desafio_PwC;
 
 public class Remover {
-	
+
 	public static void main(String[] args) {
-		
-		String s = "Hello, World!";
-		
-		for (int i = 0; i < s.length(); i++) {
-			System.out.println(s.charAt(i));
+
+		String frase = "Hello, World!";
+		String repetidas = "";
+		String letras = "";
+
+		for (int i = 0; i < frase.length(); i++) {
+			String atual = Character.toString(frase.charAt(i));
+
+			if (letras.contains(atual)) {
+				repetidas += atual;
+			}
+
+			letras += atual;
 		}
+		System.out.println(frase);
+		
+		System.out.println("\n" + repetidas);
+
 	}
 }
-
 
 //Remova todos os caracteres duplicados
 //na string abaixo: 
