468791ee60ad83a67aa137086fccd4696140b076
usuario <usuario@WINPC-03.lan>
2023-07-08 17:35:12 -0300

    Imprimir "Hello, World!" sem caracteres repetidos

--- desafio_PwC/manipulacaoDeStrings/desafio_PwC/Remover.java
+++ desafio_PwC/manipulacaoDeStrings/desafio_PwC/Remover.java
@@ -4,22 +4,25 @@ public class Remover {
 
 	public static void main(String[] args) {
 
-		String frase = "Hello, World!";
+		String input = "Hello, World!";
 		String repetidas = "";
 		String letras = "";
-
-		for (int i = 0; i < frase.length(); i++) {
-			String atual = Character.toString(frase.charAt(i));
+		String output = "";
+		
+		for (int i = 0; i < input.length(); i++) {
+			String atual = Character.toString(input.charAt(i));
 
 			if (letras.contains(atual)) {
 				repetidas += atual;
-			}
+			}else {output += atual;}
 
 			letras += atual;
 		}
-		System.out.println(frase);
+		System.out.println("\n A frase original é: "+input);
 		
-		System.out.println("\n" + repetidas);
+		System.out.println("\n A frase sem caracteres repetidos é: "+output);
+		
+		System.out.println("\n Os caracteres que se repetem são: " + repetidas);
 
 	}
 }
