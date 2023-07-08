29064cfebfb8f9c4cb95077f9f8d7c2e32b66c33
usuario <usuario@WINPC-03.lan>
2023-07-08 10:51:43 -0300

    Teste para verificação da lógica

--- desafio_PwC/manipulacaoDeStrings/desafio_PwC/Reverter.java
+++ desafio_PwC/manipulacaoDeStrings/desafio_PwC/Reverter.java
@@ -1,13 +1,30 @@
 package desafio_PwC;
 
 public class Reverter {
-	
+
 	public static void main(String[] args) {
-		
+
+		String input[] = { " hello", " world", " OpenAI" };
+		int j = 0;
+
+		for (int i = 0; i <= 2; i++) {
+			System.out.printf(input[j]);
+			j++;
+		}
+		j = 2;
+		System.out.println("");
+		for (int k = 0; k <= 2; k++) {
+			System.out.printf(input[j]);
+			j--;
+		}
 	}
 }
 
+//int[] something = { 0, 1, 2 ,3, };
+//System.out.println(something[2]);
 
-/*1. Reverta a ordem das palavras nas frases, mantendo a ordem das pelavras.
-a. Input "Hello, World! OpenAI is amazing."
-b Output "amazing is OpenAI World! Hello,"*/
+/*
+ * 1. Reverta a ordem das palavras nas frases, mantendo a ordem das pelavras. a.
+ * Input "Hello, World! OpenAI is amazing." b Output
+ * "amazing is OpenAI World! Hello,"
+ */
