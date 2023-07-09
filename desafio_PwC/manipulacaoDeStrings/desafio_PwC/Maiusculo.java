52c1a4941858da95382e17c3a755fae1b65422eb
usuario <usuario@WINPC-03.lan>
2023-07-08 21:33:31 -0300

    Commit initial

---
+++ desafio_PwC/manipulacaoDeStrings/desafio_PwC/Maiusculo.java
@@ -0,0 +1,16 @@
+package desafio_PwC;
+
+public class Maiusculo {
+	
+	public static void main(String[] args) {
+		
+		String input = "hello. how are you? i'm fine, thank you.!";
+		
+		System.out.println(input.toUpperCase());
+	}
+}
+
+
+//Coloque em maiúsculo a primeira letra de cada frase
+//Input: "hello. how are you? i'm fine, thank you."
+//Output: "Hello. How are you? I'm fine, thank you."
