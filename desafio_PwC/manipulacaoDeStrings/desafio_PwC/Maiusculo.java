9dcc6c9d40d601e064672955793f24ab83477a0a
usuario <usuario@WINPC-03.lan>
2023-07-08 22:56:41 -0300

    Construção do Output utilizando os caracteres capitalizados

--- desafio_PwC/manipulacaoDeStrings/desafio_PwC/Maiusculo.java
+++ desafio_PwC/manipulacaoDeStrings/desafio_PwC/Maiusculo.java
@@ -1,26 +1,29 @@
 package desafio_PwC;
 
 public class Maiusculo {
-	
+
 	public static void main(String[] args) {
-		
+
 		String input = "hello. how are you? i'm fine, thank you.";
-		String acum = "";
+		char acum = ' ';
 		String output = "";
-		
-		for (int i = 0; i <= input.length() -1; i++) {
+
+		for (int i = 0; i <= input.length() - 1; i++) {
 			input.charAt(i);
-			
+
 			if (i == 0 || i == 7 || i == 20) {
-				acum += input.charAt(i);
-				output = acum.toUpperCase();
-			} 
+				acum = input.charAt(i);
+				acum = Character.toUpperCase(acum);
+				output += acum;
+			} else {
+				output += input.charAt(i);
+			}
 		}
-		
+
 		System.out.println(output);
-		
-	}	
-	
+
+	}
+
 }
 
 //0, 7, 20, 
