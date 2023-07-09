528e30938b09ea911c9ba5c78e5ecc0443b36666
usuario <usuario@WINPC-03.lan>
2023-07-09 12:14:38 -0300

    Commit final

--- desafio_PwC/manipulacaoDeStrings/desafio_PwC/Anagrama.java
+++ desafio_PwC/manipulacaoDeStrings/desafio_PwC/Anagrama.java
@@ -7,13 +7,15 @@ public class Anagrama {
 	public static void main(String[] args) {
 		
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Verifique se as palavras s„o anagramas");
+		System.out.println("Verifique se as palavras s„o anagramas\n");
 		
+		System.out.printf("Digite a primeira palavra: ");
 		String input = sc.next();
+		System.out.printf("Digite a segunda palavra: ");
 		String input2 = sc.next();
 		
-		if(isAnagrama(input, input2))System.out.println("… anagrama");
-		else System.out.println("N„o È anagrama");
+		if(isAnagrama(input, input2))System.out.println("\n… anagrama!");
+		else System.out.println("\nN„o È anagrama!");
 		
 		sc.close();
 	}
